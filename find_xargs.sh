6490ee30b0c0516536bcf3e21a9af2d9a82f6f22
ryo <sigaryoutakaki@gmail.com>
2020-02-29 19:07:49 +0900

    sadfsf

---
+++ find_xargs.sh
@@ -0,0 +1,8 @@
+find . -name "*.log" | xargs rm -fv
+find . -name "*.log" -exec rm -fv {} \;
+
+## ドライラン　.. 生成したコマンドを確認する
+find . -name "*.log" | xargs -p rm -fv
+
+# コマンドに引数が2個以上必要なとき、指定する必要あり
+find . -name "*.log" | xargs -i cp {} /tmp/.
