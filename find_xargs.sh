ba411062b0b0b38781620c8cf96fd0c09915ddbb
ryo <sigaryoutakaki@gmail.com>
2020-03-01 15:59:55 +0900

    一括置換コマンド

--- find_xargs.sh
+++ find_xargs.sh
@@ -6,3 +6,7 @@ find . -name "*.log" | xargs -p rm -fv
 
 # コマンドに引数が2個以上必要なとき、指定する必要あり
 find . -name "*.log" | xargs -i cp {} /tmp/.
+
+
+# 一括置換
+find . -name "*.jmx" | xargs sed -i '' "s/xxxxx/localhsot/g"
